dc6ff2cd2d9d32c9c8310eeb71a51a3107a7d8ef
motherchod-code <sreejanpatra@gmail.com>
2026-08-08 14:28:03 +0530

    Update mega.js

--- mega.js
+++ mega.js
@@ -2,7 +2,7 @@ import { Storage } from 'megajs';
 
 const auth = {
     email: process.env.MEGA_EMAIL || 'oyysreejan@gmail.com',
-    password: process.env.MEGA_PASSWORD || 'sreejan@900',
+    password: process.env.MEGA_PASSWORD || 'sreejan9000',
     userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246'
 };
 
